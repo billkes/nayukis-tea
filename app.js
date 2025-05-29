0334893527218c52885c110a2c6b812cab8cbfc8
billkes <2294880530@qq.com>
2025-05-29 19:08:23 +0800

    优化页面

--- app.js
+++ app.js
@@ -3,47 +3,51 @@ App({
   onLaunch() {
     this.checkLoginStatus();
   },
-  checkLoginStatus: function(){
-    const openid= wx.getStorageSync('openid');
-    const token= wx.getStorageSync('token');
-    if(!openid || !token){
+  checkLoginStatus: function () {
+    const openid = wx.getStorageSync('openid');
+    const token = wx.getStorageSync('token');
+    if (!openid || !token) {
       //登录
       wx.login({
         success: (res) => {
           //发送res.code 到后台换取 openId, sessionKey, unionId
-          console.log("onLaunch",res);
+          // console.log("onLaunch", res);
           this.loginWithCode(res.code);
         }
-       })
-     }
+      })
+    }
   },
-  loginWithCode: function(code){
+  loginWithCode: function (code) {
     // 请求我自己的服务器 --用code换openid
     // 保存openid
-    const requsetUrl="https://adaa-110-16-109-248.ngrok-free.app/user/login";
+    const requsetUrl = "https://adaa-110-16-109-248.ngrok-free.app/user/login";
     wx.request({
-     url:requsetUrl,
-     data: {code},
-     method:'POST',
-     success:(res)=>{
-      //console.log(res);
-      if(res.statusCode == 200 && res.data.success){
-        const {openid, token} = res.data.data;
-        wx.setStorageSync('openid', openid);
-        wx.setStorageSync('token', token);
-      }
-      wx.showToast({
-        title: '登录成功',
-      });
-     },
-     fail:(err)=>{
-      //console.error(err);
-      console.error("【登录失败】");
-     },
-     complete:()=>{
+      url: requsetUrl,
+      data: {
+        code
+      },
+      method: 'POST',
+      success: (res) => {
+        //console.log(res);
+        if (res.statusCode == 200 && res.data.success) {
+          const {
+            openid,
+            token
+          } = res.data.data;
+          wx.setStorageSync('openid', openid);
+          wx.setStorageSync('token', token);
+        }
+        wx.showToast({
+          title: '登录成功',
+        });
+      },
+      fail: (err) => {
+        //console.error(err);
+        console.error("【登录失败】");
+      },
+      complete: () => {
 
-     }
+      }
     })
   }
 })
-  
--- pages/index/index.js
+++ pages/index/index.js
@@ -34,7 +34,7 @@ Page({
       current,
       source
     } = e.detail;
-    console.log(current, source);
+    // console.log(current, source);
     this.setData({
       selectedColor: current
     })
@@ -67,7 +67,7 @@ Page({
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
-        console.log(res)
+        // console.log(res)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
